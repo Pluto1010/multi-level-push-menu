17b84b596a3a8dc800f41a48abce03aff7bf8036
Ricardo Hildebrand <ricardo.hildebrand@votum.de>
2015-03-30 16:33:55 +0200

    swipe control changes for scrolling on touch devices

--- jquery.multilevelpushmenu.js
+++ jquery.multilevelpushmenu.js
@@ -1109,14 +1109,8 @@
 				return check;
 			}
 
-			if( mobileCheck() ) {
-				clickEventType = 'touchend';
-				dragEventType = 'touchmove';
-			}
-			else {
-				clickEventType = 'click';
-				dragEventType = 'mousedown';
-			}
+			clickEventType = 'click';
+			dragEventType = 'mousedown';
 
 			// Invoke called method or init
 			if ( methods[options] ) {
